ec4e103fc6c26683db5ee3e4971e2643819284f6
Jacob MacWilliams <jmw@dc.isc-konstanz.de>
2021-12-01 19:44:02 +0100

    Move select model method to parse_conf module.

--- bin/train.py
+++ bin/train.py
@@ -1,5 +1,5 @@
 from lib.prep_data import prepare_data
-from lib.parse_confs import parse_kwargs
+from lib.parse_confs import parse_kwargs, select_model
 import importlib
 import os
 
@@ -69,38 +69,6 @@ def save_model(model):
     os.chdir("..\\results")
     for component_name, model in model.model.items():
         model.save(component_name)
-
-
-def select_model(name):
-    """returns conf, module, and class names for a selected model
-
-    :param name: string indicating the selected model
-    :type name: str
-
-    :return kwargs: dictionary containing the conf, module, and
-    class names associated with the selected model
-    :rtype kwargs: dict
-
-    ..notes:: This function explicitely defines the convention that
-    all models are assumed to end in 'gan' (not case sensitive) and
-    be preceded by one word (e.g. TimeGAN, time_gan, time_GAN).
-    """
-
-    latent_target = ''.join(name.split('_')).upper()
-    models = []
-    for model_file in os.listdir('..\\conf'):
-
-        model_name = os.path.splitext(model_file)[0]
-        models.append(model_name)
-        latent_name = ''.join(model_name.split('_')).upper()
-        if latent_name == latent_target:
-            model_file = os.path.join('..\\conf', model_file)
-            return parse_kwargs(file=model_file, section='Aliases')
-        else:
-            continue
-
-    raise NotImplementedError('Please choose one of the following implemented'
-                                'models:\n' + '\n'.join(models))
 
 
 if __name__ == '__main__':
--- lib/parse_confs.py
+++ lib/parse_confs.py
@@ -1,5 +1,6 @@
 from configparser import ConfigParser
 import json
+import os
 
 
 def parse_kwargs(file, section):
@@ -24,3 +25,35 @@ def parse_kwargs(file, section):
         kwargs[label] = json.loads(item)
 
     return kwargs
+
+
+def select_model(name):
+    """returns conf, module, and class names for a selected model
+
+    :param name: string indicating the selected model
+    :type name: str
+
+    :return kwargs: dictionary containing the conf, module, and
+    class names associated with the selected model
+    :rtype kwargs: dict
+
+    ..notes:: This function explicitely defines the convention that
+    all models are assumed to end in 'gan' (not case sensitive) and
+    be preceded by one word (e.g. TimeGAN, time_gan, time_GAN).
+    """
+
+    latent_target = ''.join(name.split('_')).upper()
+    models = []
+    for model_file in os.listdir('..\\conf'):
+
+        model_name = os.path.splitext(model_file)[0]
+        models.append(model_name)
+        latent_name = ''.join(model_name.split('_')).upper()
+        if latent_name == latent_target:
+            model_file = os.path.join('..\\conf', model_file)
+            return parse_kwargs(file=model_file, section='Aliases')
+        else:
+            continue
+
+    raise NotImplementedError('Please choose one of the following implemented'
+                                'models:\n' + '\n'.join(models))
